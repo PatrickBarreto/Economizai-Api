896d552f6a1d8b593ea5f881252830770e9b52d0
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-02-15 21:19:38 -0300

    Rename file and implement the comparation method

---
+++ Api/Controller/ShoppingLists/ShoppingListProductOptions/ShoppingListProductOptions.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Api\Controller\ShoppingLists\ShoppingListProductOptions;
+
+use Api\Common\MathOperations\Comparations;
+use Api\Models\Products\Product;
+use Api\Models\ShoppingLists\BondShoppingListProducts\ShoppingListProducts;
+use Api\Models\ShoppingLists\ShoppingListExecution;
+use Api\Models\ShoppingLists\ShoppingListProductOptions\ShoppingListProductOptions as ShoppingListProductExecutionModel;
+
+use Exception\Exception;
+use Http\Request\Request;
+
+class ShoppingListProductOptions {
+
+    public static function addOptionProduct(Request $request){
+        
+        $executionHash =        isset($request->getPathParams()['hash']) ? $request->getPathParams()['hash'] : '';
+        $productListId =        isset($request->getPathParams()['productListId']) ? (int)$request->getPathParams()['productListId'] : 0;
+
+        $productOptionId =      isset($request->getBody()->products_id) ? $request->getBody()->products_id : 0;
+        $productOptionBrandId = isset($request->getBody()->brands_id) ? $request->getBody()->brands_id : 0;
+        $quantity =             isset($request->getBody()->quantity) ? $request->getBody()->quantity : 0;
+        $typeDescription =      isset($request->getBody()->type_description) ? $request->getBody()->type_description : 0;
+        $wheight =              isset($request->getBody()->wheight) ? $request->getBody()->wheight : 0;
+        $unitMensure =          isset($request->getBody()->unit_mensure) ? $request->getBody()->unit_mensure : 0;
+        $price =                isset($request->getBody()->price) ? $request->getBody()->price : 0;
+
+        if(!$executionHash || !$productListId || !$productOptionId || !$productOptionBrandId || !$quantity || !$typeDescription || !$wheight || !$unitMensure || !$price){
+            Exception::throw('Miss information', 200);
+        }
+
+        $shoppingListExecution =   (new ShoppingListExecution())->findExecutionsByHash($executionHash);
+       
+
+        return (new ShoppingListProductExecutionModel())->create([
+                                                    $shoppingListExecution->getProperty('execution_hash'),
+                                                    $productListId, 
+                                                    $productOptionId,
+                                                    $productOptionBrandId,
+                                                    $typeDescription,
+                                                    $wheight,
+                                                    $unitMensure,
+                                                    $quantity,
+                                                    $price
+                                                ]);
+
+    }
+
+    public static function listAllOptionByHashAndProductsListId(Request $request){
+
+        $executionHash =    isset($request->getPathParams()['hash']) ? $request->getPathParams()['hash'] : '';
+        $productListId =    isset($request->getPathParams()['productListId']) ? (int)$request->getPathParams()['productListId'] : 0;
+        
+        $options =   (new ShoppingListProductExecutionModel())->listOptionsByProductList($executionHash, (int)$productListId);
+
+        
+        if($options){
+            return $options;
+        }
+       
+        Exception::throw('Option product list not found', 200);
+
+    }
+
+    public static function findOptionByProductOptionId(Request $request){
+
+        $executionHash =   isset($request->getPathParams()['hash']) ? $request->getPathParams()['hash'] : 0;
+        $productOptionId = isset($request->getPathParams()['productOptionId']) ? (int)$request->getPathParams()['productOptionId'] : 0;
+        
+        $details =   (new ShoppingListProductExecutionModel())->findDetailByProductOptionId($productOptionId, $executionHash);
+
+        if($details){
+            return $details;
+        }
+       
+        Exception::throw('Option product list not found', 200);
+
+    }
+
+    public static function delete(Request $request){
+        
+        $executionHash =   isset($request->getPathParams()['hash']) ? $request->getPathParams()['hash'] : 0;
+        $productOptionId = isset($request->getPathParams()['productOptionId']) ? (int)$request->getPathParams()['productOptionId'] : 0;
+        
+        $productOption = (new ShoppingListProductExecutionModel)->findProductOption($productOptionId, $executionHash);
+
+        if($productOption){
+            $productOption->delete();
+            return true;
+        }
+
+        Exception::throw('Product option not found', 200);
+        
+    } 
+
+    public static function findBestOption(Request $request) {
+        
+        $executionHash =  isset($request->getPathParams()['hash']) ? $request->getPathParams()['hash'] : '';
+        $productListId =  isset($request->getPathParams()['productListId']) ? (int)$request->getPathParams()['productListId'] : 0;
+        
+        $productIten = (new ShoppingListProducts)->findBondsById($productListId);
+        $products = (new Product)->findProduct($request->currentUser, $productIten->getProperty('products_id'));
+        $options = (new ShoppingListProductExecutionModel())->listOptionsByProductList($executionHash, (int)$productListId);
+        
+        $data = [];
+        $data['calcType'] = $products['unit_mensure'];
+
+        foreach($options as $option){
+            $data['itensToCopare'][] = [
+                'id'=>$option['id'],
+                'name'=>$option['products_id'], 
+                'brand_id'=>$option['brands_id'], 
+                'price'=>(float)$option['price'], 
+                'weight'=>$option['quantity'] * (Comparations::convertOptionToUnitMensure($option, $products['unit_mensure']))
+            ];
+        }
+
+       return Comparations::compare($data);
+       
+    }
+}
---
+++ Api/Models/ShoppingLists/ShoppingListProductOptions/ShoppingListProductOptions.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Api\Models\ShoppingLists\ShoppingListProductOptions;
+
+use DataBase\CrudExtension;
+
+class ShoppingListProductOptions extends CrudExtension{
+
+    public static string $table = 'bond_shopping_lists_products_options';
+
+    protected int $id;
+    protected string $shopping_lists_execution_hash;
+    protected int $bond_shopping_lists_products_id;
+    protected int $products_id;
+    protected int $brands_id;
+    protected string $type_description;
+    protected float $wheight;
+    protected string $unit_mensure;
+    protected int $quantity;
+    protected float $price;
+
+    public function create(array $values) {
+
+        //Antes de inserir isso deve verificar se o produto e a marca são da mesma categoria
+        
+        return $this->insert->setFields([
+                                        'shopping_lists_execution_hash',
+                                        'bond_shopping_lists_products_id',
+                                        'products_id',
+                                        'brands_id',
+                                        'type_description',
+                                        'wheight',
+                                        'unit_mensure',
+                                        'quantity',
+                                        'price'
+                                        ])->setValues($values)->runQuery();
+    }
+
+
+    public function listOptionsByProductList(string $executionHash, int $productListId){
+        return $this->select->setWhere('shopping_lists_execution_hash = "'.$executionHash.'" 
+                                        AND bond_shopping_lists_products_id = '.$productListId)
+                            ->fetchAssoc(true);
+    }
+
+
+    public function findDetailByProductOptionId(int $id, string $executionHash){
+        return $this->select->setFields(
+                                [
+                                    'bond_shopping_lists_products_options.id as OptionId',
+                                    'bond_shopping_lists_products_options.products_id as OptionProductId',
+                                    'products.name as OptionProductName',
+                                    'brands.name as OptionProductBrandName',
+                                ])
+                    ->setInnerJoin(
+                                    ['table'=> 'bond_shopping_lists_products_options','ON'=> 'bond_shopping_lists_products_id'],
+                                    ['table'=> 'bond_shopping_lists_products']
+                                )
+                    ->setInnerJoin(
+                                    ['table'=> 'bond_shopping_lists_products_options','ON'=> 'products_id'],
+                                    ['table'=> 'products']
+                                )
+                    ->setInnerJoin(
+                                    ['table'=> 'bond_shopping_lists_products_options','ON'=> 'brands_id'],
+                                    ['table'=> 'brands']
+                                )
+                    ->setWhere('bond_shopping_lists_products_options.id = '.$id.' 
+                                AND bond_shopping_lists_products_options.shopping_lists_execution_hash = "'.$executionHash.'"')
+                    ->fetchAssoc(false);
+    }
+
+    public function findProductOption(int $id, string $executionHash){
+        return $this->select->setWhere('bond_shopping_lists_products_options.id = '.$id.' 
+                                        AND bond_shopping_lists_products_options.shopping_lists_execution_hash = "'.$executionHash.'"')
+                            ->fetchObject(false, self::class);
+    }
+
+    public function delete(){
+        return $this->delete->setWhere('bond_shopping_lists_products_options.id = '.$this->id.' 
+                                        AND bond_shopping_lists_products_options.shopping_lists_execution_hash = "'.$this->shopping_lists_execution_hash.'"')
+                            ->fetchObject(false, self::class);
+    }
+}
