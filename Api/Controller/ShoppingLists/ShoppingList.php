bea5f06248cf26d3e5877cc41aa2bb7fbfb4ef74
Patrick Barreto <contato.patrickbarreto@gmail.com>
2025-03-04 15:07:10 -0300

    C-ECO-7 fix: shopping list adjust

--- Api/Controller/ShoppingLists/ShoppingList.php
+++ Api/Controller/ShoppingLists/ShoppingList.php
@@ -42,7 +42,8 @@ class ShoppingList {
         $shoppingList = $shoppingListRepository->findShoppingList($request->currentUser, $request->getPathParams()['id'], ['id','accounts_id', 'name', 'type']);
         
         if($shoppingList) {
-            $executions =  $shoppingListExecutionRepository->findExecutionsByListId($request->getPathParams()['id']);
+            $shoppingList['executions'] = [];
+            $executions = (array)$shoppingListExecutionRepository->findExecutionsByListId($request->getPathParams()['id']);
             if($executions){
                 $shoppingList['executions'] = array_map(fn($execution)=>['id'=>$execution->getProperty('id'), 'created'=>date('Y-m-d',$execution->getProperty('created'))],$executions);
             }
