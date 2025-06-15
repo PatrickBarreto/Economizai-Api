db95fce70e86996d7fbe901dfdb145124647679a
Patrick Barreto <contato.patrickbarreto@gmail.com>
2025-06-15 13:51:07 -0300

    ECO-B-16 feat create brands with categories bonds

--- Api/Models/Categories/BondCategoryBrands/CategoryBrandsRepository.php
+++ Api/Models/Categories/BondCategoryBrands/CategoryBrandsRepository.php
@@ -6,8 +6,21 @@ use DataBase\RepositoryConnection\Repository;
 
 class CategoryBrandsRepository extends Repository{
 
-    public function createBond(array $values) {
-        return $this->insert()->setFields(['categories_id', 'brands_id'])->setValues($values)->runQuery();
+    public function createBond(int $brandId, array $categories) {
+      $bondedValues = [];
+      foreach($categories as $category){
+        array_push($bondedValues, [$category,$brandId]);
+      }
+      
+      return $this->insert()->setFields(['categories_id','brands_id'])->setValues($bondedValues)->runQuery();
+    }
+
+    public function findBondCategoriesByBrandId(int $brands_id){
+      return $this->select()
+        ->setFields(['bond_categories_brands.id as bondId', 'categories.id' , 'categories.name'])
+        ->setInnerJoin(['table'=>'bond_categories_brands', 'ON'=>'categories_id'], ['table'=>'categories'])
+        ->setWhere('bond_categories_brands.brands_id = '.$brands_id)
+        ->fetchAssoc(true);
     }
 
     public function findBonds($brands_id, $categories_id, array $fields = ['*']) {
@@ -24,6 +37,14 @@ class CategoryBrandsRepository extends Repository{
 
     public function deleteAllBond(int $categorieId, string $brandsIds) {
         return $this->delete()->setWhere('categories_id = '.$categorieId.' AND brands_id IN ('.$brandsIds.')')->runQuery();
+    }
+
+      public function updateBonds(int $brandsId, array $categories) {
+      $this->delete()->setWhere('brands_id IN ('.(string)$brandsId.')')->runQuery();
+      if($categories){
+        $this->createBond($brandsId, $categories);
+      }
+      return true;
     }
 
 }
